a68f778575f031286a6894a53757232bf14e603e
ahmedaymanelalfy <ahmed.ayman.elalfy.pro@gmail.com>
2024-09-22 10:46:40 +0300

    Add Db migrations

--- AirlineWeb/Program.cs
+++ AirlineWeb/Program.cs
@@ -1,9 +1,15 @@
+using AirlineWeb.Data;
+using Microsoft.EntityFrameworkCore;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AirlineDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ConnectionStrings")));
 
 var app = builder.Build();
 
